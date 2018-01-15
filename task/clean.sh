f6a28106ff196ab94e0a4f54b62a1f799370d62e
Jerry Hamlet <jerry@logichub.com>
2018-01-15 11:45:22 -0800

    A more complicated clean.sh

--- task/clean.sh
+++ task/clean.sh
@@ -4,4 +4,16 @@ DIR_NAME=$( dirname "$0" )
 # shellcheck source=/dev/null
 source "$DIR_NAME/common.sh"
 
-rm -rvf "$DIST_DIR"
+find "$PROJECT_DIR" -type f -name '*.js' \
+  ! -path "$PROJECT_DIR/node_modules/*" \
+  ! -path "$PROJECT_DIR/src/*" \
+  -exec rm -v {} \;
+
+find "$PROJECT_DIR" -type d \
+  ! -path "$PROJECT_DIR" \
+  ! -path "$PROJECT_DIR/.git*" \
+  ! -path "$PROJECT_DIR/node_modules*" \
+  ! -path "$PROJECT_DIR/src*" \
+  ! -path "$PROJECT_DIR/task*" \
+  ! -path "$PROJECT_DIR/bin*" \
+  -exec rm -rfv {} \;
